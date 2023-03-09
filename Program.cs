78ef169154398f4bf6f54e85c0005fb045272157
HT980917 <xkr1357@naver.com>
2023-03-09 10:57:38 +0900

    modified by hyuntak

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+public class MyList
+{
+    private int[] data = { 1, 2, 3, 4, 5 };
+    public IEnumerator GetEnumerator()
+    {
+        int i = 0;
+        while (i < data.Length)
+        {
+            yield return data[i];
+            i++;
+        }
+    }
+}
+
+namespace ConsoleApp1
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new MyList();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerator it = list.GetEnumerator();
+            it.MoveNext();
+            Console.WriteLine(it.Current);      //1
+            it.MoveNext();
+            Console.WriteLine(it.Current);      //2
+            it.MoveNext();
+            Console.WriteLine(it.Current);      //3
+
+        }
+    }
+}
